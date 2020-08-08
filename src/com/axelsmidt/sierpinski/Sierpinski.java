075d3fa6d96e49991d1b77d0c7a9dc0cd1995857
Axel Smidt <axelpilot78@gmail.com>
2020-08-08 23:08:12 +0200

    Draws a Sierpinski triangle.

---
+++ src/com/axelsmidt/sierpinski/Sierpinski.java
@@ -0,0 +1,5 @@
+package com.axelsmidt.sierpinski;
+
+public class Sierpinski {
+
+}
---
+++ src/com/axelsmidt/sierpinski/SierpinskiTest.java
@@ -0,0 +1,15 @@
+package com.axelsmidt.sierpinski;
+
+import javax.swing.JFrame;
+
+public class SierpinskiTest {
+    public static void main(String[] args) {
+        JFrame app = new JFrame();
+
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setSize(1200, 1000);
+        app.setVisible(true);
+        DrawSierpinksi panel = new DrawSierpinksi();
+        app.add(panel);
+    }
+}
